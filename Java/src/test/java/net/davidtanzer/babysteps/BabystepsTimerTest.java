02504f2399f8e52b3a403eed7c969809db7d3d2e
Paul Klingelhuber <Paul.Klingelhuber@catalysts.cc>
2017-10-20 17:39:32 +0200

    step 1

--- Java/src/test/java/net/davidtanzer/babysteps/BabystepsTimerTest.java
+++ Java/src/test/java/net/davidtanzer/babysteps/BabystepsTimerTest.java
@@ -1,5 +1,6 @@
 package net.davidtanzer.babysteps;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -9,11 +10,57 @@ import static org.junit.Assert.assertTrue;
  */
 public class BabystepsTimerTest {
 
+    private BabystepsTimer babystepsTimer;
+    private BabystepsTimerTestDriver babystepsTimerTestDriver;
+
+    @Before
+    public void setUp() throws Exception {
+        babystepsTimer = new BabystepsTimer();
+        babystepsTimerTestDriver = new BabystepsTimerTestDriver(babystepsTimer);
+        babystepsTimerTestDriver.init();
+    }
+
     @Test
     public void showsTwoMinutesAfterStartingUp() throws Exception {
-        BabystepsTimerTestDriver.init();
-        Thread.sleep(50L);
-        assertTrue(BabystepsTimerTestDriver.userInterfaceContains("02:00"));
+        babystepsTimerTestDriver.waitFor(50L);
+        assertTrue(babystepsTimerTestDriver.userInterfaceContains("02:00"));
+    }
+
+    @Test
+    public void countDownAfterPressingStart() throws Exception {
+        babystepsTimerTestDriver.init();
+        babystepsTimerTestDriver.waitFor(50L);
+
+        babystepsTimerTestDriver.press("start");
+        babystepsTimerTestDriver.waitFor(1500);
+        assertTrue(babystepsTimerTestDriver.userInterfaceContains("01:59"));
+    }
+
+    @Test
+    public void stopCountdownAfterPressingStop() throws Exception {
+        babystepsTimerTestDriver.init();
+        babystepsTimerTestDriver.waitFor(50L);
+
+        babystepsTimerTestDriver.press("start");
+        babystepsTimerTestDriver.waitFor(1500);
+        assertTrue(babystepsTimerTestDriver.userInterfaceContains("01:59"));
+        babystepsTimerTestDriver.press("stop");
+        babystepsTimerTestDriver.waitFor(1000);
+        assertTrue(babystepsTimerTestDriver.userInterfaceContains("02:00"));
+    }
+
+    @Test
+    public void resetResetsTimerToInitialTime() throws Exception {
+        babystepsTimerTestDriver.init();
+        final long time = 50L;
+        babystepsTimerTestDriver.waitFor(time);
+
+        babystepsTimerTestDriver.press("start");
+        babystepsTimerTestDriver.waitFor(2500);
+        assertTrue(babystepsTimerTestDriver.userInterfaceContains("01:58"));
+        babystepsTimerTestDriver.press("reset");
+        babystepsTimerTestDriver.waitFor(1500);
+        assertTrue(babystepsTimerTestDriver.userInterfaceContains("01:59"));
     }
 
 }
--- Java/src/test/java/net/davidtanzer/babysteps/BabystepsTimerTestDriver.java
+++ Java/src/test/java/net/davidtanzer/babysteps/BabystepsTimerTestDriver.java
@@ -1,7 +1,5 @@
 package net.davidtanzer.babysteps;
 
-import org.junit.Test;
-
 import javax.swing.event.HyperlinkEvent;
 
 import static org.junit.Assert.assertTrue;
@@ -11,18 +9,27 @@ import static org.junit.Assert.assertTrue;
  */
 public class BabystepsTimerTestDriver {
 
+    private BabystepsTimer babystepsTimer;
 
-    public static boolean userInterfaceContains(String text) {
-        return BabystepsTimer.timerPane.getText().contains(text);
+    public BabystepsTimerTestDriver(BabystepsTimer babystepsTimer) {
+        this.babystepsTimer = babystepsTimer;
     }
 
-    public static void press(String command) throws InterruptedException {
-        BabystepsTimer.timerPane.getHyperlinkListeners()[0].hyperlinkUpdate(
-                new HyperlinkEvent(BabystepsTimer.timerPane, HyperlinkEvent.EventType.ACTIVATED, null, "command://" + command));
+    public boolean userInterfaceContains(String text) {
+        return babystepsTimer.timerPane.getText().contains(text);
+    }
+
+    public void press(String command) throws InterruptedException {
+        babystepsTimer.timerPane.getHyperlinkListeners()[0].hyperlinkUpdate(
+                new HyperlinkEvent(babystepsTimer.timerPane, HyperlinkEvent.EventType.ACTIVATED, null, "command://" + command));
         Thread.sleep(50L);
     }
 
-    public static void init() throws Exception {
-        BabystepsTimer.main(new String[0]);
+    public void init() throws Exception {
+        babystepsTimer.main(new String[0]);
+    }
+
+    static void waitFor(long timeInMilliseconds) throws InterruptedException {
+        Thread.sleep(timeInMilliseconds);
     }
 }
